dd382da2feb6523b10dab41394a49ade5d009316
Vishal Kolambe <vishalkolambe111@gmail.com>
2025-03-09 22:19:24 +0000

    Update main.tf

--- terraform-setup/main.tf
+++ terraform-setup/main.tf
@@ -1,7 +1,7 @@
 resource "aws_instance" "web_server" {
-  ami             = var.ami_id
-  instance_type   = var.instance_type
-  key_name        = var.key_name
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
   vpc_security_group_ids = [var.security_group_id]
 
   tags = {
